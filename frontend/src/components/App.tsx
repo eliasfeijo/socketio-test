f58b56d8971c628331bc299646242b386f1d56b1
Elias Feijó <eliasfeijo.dev@gmail.com>
2021-10-29 23:42:41 -0300

    removing unused components, and adding Home

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,31 +1,15 @@
 import React from "react";
-import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { TodosProvider } from "../contexts/TodosContext";
-import CreateTodo from "./Todo/CreateTodo";
-import ListTodo from "./Todo/ListTodo";
 import "./App.css";
 import Footer from "./Footer";
 import About from "./About";
+import Home from "./Home";
 
 const App = (): JSX.Element => (
   <Router>
     <div className="h-full flex flex-col">
-      <h1 className="pt-4 px-4 text-4xl text-center">Todo Application</h1>
-      <nav>
-        <ul className="flex justify-center space-x-2 pt-5">
-          <li>
-            <Link to="/" className="link">
-              Home
-            </Link>
-          </li>
-          <li>|</li>
-          <li>
-            <Link to="/about" className="link">
-              About
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <h1 className="pt-4 px-4 text-4xl text-center">Chat Application</h1>
       <div className="pb-4 pt-4">
         <TodosProvider>
           <Switch>
@@ -33,8 +17,7 @@ const App = (): JSX.Element => (
               <About />
             </Route>
             <Route path="/">
-              <CreateTodo />
-              <ListTodo />
+              <Home />
             </Route>
           </Switch>
         </TodosProvider>
---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = (): JSX.Element => (
+  <div>
+    <h2 className="text-center">Home</h2>
+  </div>
+);
+
+export default Home;
